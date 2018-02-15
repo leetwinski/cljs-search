98e31a32193338ae5901140668d4b7735fe90b1a
Victor Litvintsev <leetwinski@gmail.com>
2018-02-16 00:32:55 +0300

    [master] update bulk insert data for testing needs

--- elastic/prepare_data.sh
+++ elastic/prepare_data.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 i=0
-read FILE
+FILE=$1
 
-if [[ -f $FILE ]]; then 
+escape_quotes() {
+    echo $1 | sed 's|\"|\\\"|g'
+}
+
+remove_blank_lines() {
+    sed '/^\s*$|^M$/d' < $1 | cat
+}
+
+echo -e "$(remove_blank_lines "$FILE")" | if [[ -f "$FILE" ]]; then 
     while ((i++)); IFS='' read -r line || [[ -n "$line" ]]; do
         printf '{"index": {"_index":"test", "_type": "_doc", "_id": "%s"}}\n' $i
-        printf '{"text": "%s"}\n' "$line"
-    done < "$FILE"
+        printf '{"text": "%s"}\n' "$(escape_quotes "$line")"
+    done 
 else
     echo "file $FILE doesn't exist" >&2
     exit 1
